904e457534fe18e1cddefb8c4b14f04de5541b54
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-22 19:43:44 +0300

    Create configure_ufw.sh

---
+++ project-13-linux-firewall-security-hardening-lab/configure_ufw.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo ufw default deny incoming
+
+sudo ufw default allow outgoing
+
+sudo ufw allow 22/tcp
+
+sudo ufw allow 443/tcp
+
+sudo ufw enable
+
+echo "Firewall configured successfully."
